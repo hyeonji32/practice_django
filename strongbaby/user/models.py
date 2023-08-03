810f8cea683adee6050ef037c52c3385d2a05614
hyeonji32 <131431174+hyeonji32@users.noreply.github.com>
2023-08-03 20:08:02 +0900

    #9:user 생성 api 개발

--- strongbaby/user/models.py
+++ strongbaby/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    user_id = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    def __init__(self, user_id, password, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_id = user_id
+        self.password = password
--- strongbaby/user/views.py
+++ strongbaby/user/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+
+from .models import User
+
 
 # Create your views here.
 
@@ -8,3 +12,13 @@ def hello(self):
     print("aaa")
     print(self)
     return HttpResponse("안녕하세요")
+
+
+def signup(request):
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        user_id = data['user_id']
+        password = data['password']
+        user = User(user_id, password)
+        user.save()
+    return HttpResponse("ok")
